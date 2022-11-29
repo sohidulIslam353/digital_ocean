4a8b0e3c069cda0d447169dec92210852b2ebb79
sohidulislam353 <sohidulislam353@gmail.com>
2022-11-29 16:45:39 +0600

    changes

--- public/index.php
+++ public/index.php
@@ -20,8 +20,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-
-
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -33,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -46,7 +44,7 @@ require __DIR__.'/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
